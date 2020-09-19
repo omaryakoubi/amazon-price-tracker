b107d814c2bc1ba95e6b4b79138bee34406343db
omaryakoubi <yakoubiomar2001@gmail.com>
2020-09-19 11:38:10 +0100

    working on

---
+++ main.py
@@ -0,0 +1,6 @@
+import requests
+
+URL = input('paste here the link of your amazon product: ')
+
+# to get your user agent go to google and search for "my user agent".
+headers = {"user-agent" : 'paste you user_agent here'}
