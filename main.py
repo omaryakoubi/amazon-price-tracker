09701efb41d25914dabf59a6075bc4ebb53adc0e
omaryakoubi <yakoubiomar2001@gmail.com>
2020-09-19 18:17:58 +0100

    debugging 😠

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import requests
 from bs4 import BeautifulSoup
 
-URL = input("paste here the link of your amazon product: ")
+URL = 'https://www.amazon.it/Apple-iPhone-11-64GB-Nero/dp/B07XS2ZR1K/ref=sr_1_5?__mk_it_IT=%C3%85M%C3%85%C5%BD%C3%95%C3%91&dchild=1&keywords=iphone+11&qid=1600534359&sr=8-5'
+
+# YOUR_IDEAL_PRICE = int(input("please enter the idel price for you :"))
 
 # PLEASE REPLACE THIS CODE WITH YOUR USER AGENT :
 # TO GET YOUR USER AGENT GO TO YOUR BROWSER AND SEARCH FOR "MY USER AGENT" AND REPLACE IT WITH YOURS.
@@ -11,7 +13,8 @@ page = requests.get(URL,headers = headers)
 
 soup = BeautifulSoup(page.content, 'html.parser')
 
-title = soup.find(id="productTitle")
-price = soup.find(id="priceblock_ourprice")
+title = soup.find(id="productTitle").get_text()
+price = soup.find(id="priceblock_ourprice").get_text()
+converted_price =  price[0:5]
 
-print(price)
+print(float(converted_price))
