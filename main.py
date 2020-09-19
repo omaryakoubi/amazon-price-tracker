4afb7ee8bc68d5e0e44a6545e807e9d5a2fc2ced
omaryakoubi <yakoubiomar2001@gmail.com>
2020-09-19 11:44:51 +0100

    stuck for now

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 import requests
+from bs4 import BeautifulSoup
 
 URL = input('paste here the link of your amazon product: ')
 
 # to get your user agent go to google and search for "my user agent".
 headers = {"user-agent" : 'paste you user_agent here'}
+
+page = requests.get(URL,headers = headers)
+
+soup = BeautifulSoup(page.content, 'html.parser')
