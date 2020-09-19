37a063495a3712789b65e8776a3da5e6bd84b611
omaryakoubi <yakoubiomar2001@gmail.com>
2020-09-19 18:03:11 +0100

    amazon-price-tracker

--- main.py
+++ main.py
@@ -3,7 +3,8 @@ from bs4 import BeautifulSoup
 
 URL = input("paste here the link of your amazon product: ")
 
-# to get your user agent go to google and search for "my user agent".
+# PLEASE REPLACE THIS CODE WITH YOUR USER AGENT :
+# TO GET YOUR USER AGENT GO TO YOUR BROWSER AND SEARCH FOR "MY USER AGENT" AND REPLACE IT WITH YOURS.
 headers = {"user-agent" : "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.83 Safari/537.36"}
 
 page = requests.get(URL,headers = headers)
