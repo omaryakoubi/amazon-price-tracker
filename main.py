44bf54681a9ba63d827e0756f4f20c79d9fb897a
omaryakoubi <yakoubiomar2001@gmail.com>
2020-09-19 17:59:12 +0100

    getting the title and the price of product done

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
 import requests
 from bs4 import BeautifulSoup
 
-URL = input('paste here the link of your amazon product: ')
+URL = input("paste here the link of your amazon product: ")
 
 # to get your user agent go to google and search for "my user agent".
-headers = {"user-agent" : 'paste you user_agent here'}
+headers = {"user-agent" : "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.83 Safari/537.36"}
 
 page = requests.get(URL,headers = headers)
 
 soup = BeautifulSoup(page.content, 'html.parser')
+
+title = soup.find(id="productTitle")
+price = soup.find(id="priceblock_ourprice")
+
+print(price)
