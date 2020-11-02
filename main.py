7b98d7cf61ab6193e49f1a8395f719549017dfe3
omaryakoubi <yakoubiomar2001@gmail.com>
2020-11-02 21:33:29 +0100

    almost done

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
 import requests
 from bs4 import BeautifulSoup
 import smtplib 
+import time
 
 URL = 'https://www.amazon.it/Apple-iPhone-11-64GB-Nero/dp/B07XS2ZR1K/ref=sr_1_5?__mk_it_IT=%C3%85M%C3%85%C5%BD%C3%95%C3%91&dchild=1&keywords=iphone+11&qid=1600534359&sr=8-5'
 
+# URL = str(Enter the amazon product link here)
 # user_ideal_price = int(input("please enter the ideal price for you :"))
 # user_email = str("please enter your address email")
 
-# PLEASE REPLACE THIS CODE WITH YOUR USER AGENT :
-# TO GET YOUR USER AGENT GO TO YOUR BROWSER AND SEARCH FOR "MY USER AGENT" AND REPLACE IT WITH YOURS.
 headers = {"user-agent" : "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.83 Safari/537.36"}
 
 def check_price():
@@ -21,9 +21,9 @@ def check_price():
     converted_price =  float(price[0:5].replace(',','.'))
 
     if (converted_price < user_ideal_price):
-        send_mail()
+        send_email()
 
-def send_mail() :
+def send_email() :
     server = smtplib.SMTP("smtp.gmail.com",587)
     server.ehlo()
     server.starttls()
@@ -31,6 +31,8 @@ def send_mail() :
     
     server.login("email","password")
     subject = "Price fell down!"
-    body = f"check the price on amazon {user_ideal_price}"
+    body = f"Check the price on amazon {URL}"
     msg = f"subject:{subject}\n\n{body}"
-    server.sendmail("email",user_email)
+    server.sendmail("email", user_email, msg)
+
+    server.quit()
